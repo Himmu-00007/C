bfd604f5c72e34bfbcc7b63e127b8013f674771f
Himmu-00007 <himmu00007@gmail.com>
2026-09-20 07:43:34 +0545

    Add program to determine pass or fail based on marks

---
+++ Pass or Fail/Himmu8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+	float m;
+	
+	printf("Enter the marks: ");
+	scanf("%f",&m);
+	
+	
+	if (m>=40)
+	{
+	
+      printf ("\nPass");
+	}
+	else
+	{  
+	  printf ("\nFail");
+    }
+	
+ return 0;	
+}
